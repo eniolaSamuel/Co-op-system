7258b88263652feacf23cc5fbc574868054aad34
Eniola Samuel <theeniolasamuel@gmail.com>
2025-01-22 09:40:03 +0100

    asset model set up

---
+++ src/main/java/com/cooperative/system/cooperative_system/data/models/Asset.java
@@ -0,0 +1,26 @@
+package com.cooperative.system.cooperative_system.data.models;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+public class Asset {
+    @Id
+    private String id;
+
+    private String memberId;
+    private String assetName;
+    private String assetDescription;
+    private BigDecimal assetPrice;
+    private BigDecimal amountPaid;
+    private String paymentStatus;
+
+    @ElementCollection
+    private List<BigDecimal> installmentPayments;
+
+
+}
