4763397a49fccaee6b9d9b1247cb33b55571c433
Eniola Samuel <theeniolasamuel@gmail.com>
2025-01-22 10:06:59 +0100

    member update request set up

--- src/main/java/com/cooperative/system/cooperative_system/dtos/requests/MemberUpdateRequest.java
+++ src/main/java/com/cooperative/system/cooperative_system/dtos/requests/MemberUpdateRequest.java
@@ -1,4 +1,13 @@
 package com.cooperative.system.cooperative_system.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class MemberUpdateRequest {
+    private String fullname;
+    private String phoneNumber;
+    private String department;
+    private String maritalStatus;
+
+
 }
