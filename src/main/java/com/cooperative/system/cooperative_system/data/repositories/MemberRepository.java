febdf842d02901611441d7b2636c63bef3c21df9
Eniola Samuel <theeniolasamuel@gmail.com>
2025-01-22 09:49:43 +0100

    member repo set up

---
+++ src/main/java/com/cooperative/system/cooperative_system/data/repositories/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.cooperative.system.cooperative_system.data.repositories;
+
+import com.cooperative.system.cooperative_system.data.models.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+    Optional<Member> findByEmail(String email);
+}
