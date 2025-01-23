867528556c92a01fae9b65baba5e62c88534c9b7
Eniola Samuel <theeniolasamuel@gmail.com>
2025-01-23 11:27:15 +0100

    member service implementation set up

--- src/main/java/com/cooperative/system/cooperative_system/controllers/PaystackController.java
+++ src/main/java/com/cooperative/system/cooperative_system/controllers/PaystackController.java
@@ -2,11 +2,13 @@ package com.cooperative.system.cooperative_system.controllers;
 
 
 import com.cooperative.system.cooperative_system.dtos.requests.AddCardRequest;
+import com.cooperative.system.cooperative_system.dtos.responses.PaystackTransactionResponse;
 import com.cooperative.system.cooperative_system.services.interfaces.PaystackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +21,12 @@ public class PaystackController {
     @PostMapping("/{memberId}/add-card")
     public ResponseEntity<String> addCard(@PathVariable UUID memberId, @RequestBody AddCardRequest request) {
         return ResponseEntity.ok(paystackService.addCard(memberId, request));
+    }
+
+    @PostMapping("/deposit/{memberId}")
+    public ResponseEntity<PaystackTransactionResponse> initiateDeposit(@PathVariable UUID memberId, @RequestParam BigDecimal amount) {
+        PaystackTransactionResponse response = paystackService.initiateDeposit(memberId, amount);
+        return ResponseEntity.ok(response);
     }
 }
 
